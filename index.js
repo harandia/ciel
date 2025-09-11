40047d9054d5ed13f2666405118a41b10e956486
harandi <hugo.arandia.t@gmail.com>
2025-09-11 11:29:53 +0200

    electron setup; minor visualization errors fixed

---
+++ index.js
@@ -0,0 +1,29 @@
+const { BrowserWindow, app, Menu } = require('electron');
+const path = require('node:path');
+
+function createWindow() {
+	const win = new BrowserWindow({
+		title: 'ciel',
+		icon: path.join(__dirname, 'icon', 'ciel-logo.png'),
+		width: 1000,
+		height: 500,
+
+		minHeight: 800,
+		minWidth: 500,
+
+		show: false,
+	});
+
+	win.maximize();
+	Menu.setApplicationMenu(null);
+
+	win.loadFile('index.html');
+
+	win.once('ready-to-show', () => {
+		win.show();
+	});
+}
+
+app.whenReady().then(() => {
+	createWindow();
+});
