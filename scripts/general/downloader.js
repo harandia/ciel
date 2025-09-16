ba03a25ac44e234e995c61b1c2edca2303a996e2
harandi <hugo.arandia.t@gmail.com>
2025-09-16 23:12:37 +0200

    downloadImage method

--- scripts/general/downloader.js
+++ scripts/general/downloader.js
@@ -1,16 +1,36 @@
 const fs = require('node:fs/promises');
+const path = require('node:path');
+const url = require('node:url');
+// @ts-ignore
+const mimeType = require('stream-mime-type');
 
 /**
- * Downloads the content of sourceURL in destPath.
+ * Given a https (or http) or file:// URL, it copies the contents of the URL in the indicated file path.
  * @param {string} sourceURL
  * @param {string} destPath
  */
-const download = async function (sourceURL, destPath) {
+const downloadImage = async function (sourceURL, destPath) {
+	let sourceFile;
+	let sourceResponse;
+	let source;
+
 	let destFile;
 	try {
-		const downloadResponse = await fetch(sourceURL);
-		if (!downloadResponse.ok || downloadResponse.body === null) {
-			throw new RequestError('Request to download the file failed.');
+		if (sourceURL.startsWith('file://')) {
+			sourceFile = await fs.open(url.fileURLToPath(sourceURL));
+			source = sourceFile.readableWebStream();
+		} else if (sourceURL.startsWith('https://') || sourceURL.startsWith('http://')) {
+			sourceResponse = await fetch(sourceURL);
+			if (!sourceResponse.ok || sourceResponse.body === null) {
+				throw new RequestError();
+			}
+			source = sourceResponse.body;
+		} else {
+			throw new InvalidURLError();
+		}
+		const { stream, mime } = await mimeType.getMimeType(source);
+		if (!mime.startsWith('image/')) {
+			throw new FileTypeError();
 		}
 		destFile = await fs.open(destPath, 'ax');
 		const fileWriter = new WritableStream({
@@ -18,21 +38,26 @@ const download = async function (sourceURL, destPath) {
 				await destFile.write(chunk);
 			},
 		});
-		await downloadResponse.body.pipeTo(fileWriter).catch((err) => {
-			try {
-				fs.unlink(destPath);
+		await stream.pipeTo(fileWriter).catch((err) => {
+			fs.unlink(destPath).catch((err) => {
 				throw err;
-			} catch (err) {
-				throw err;
-			}
+			});
+			throw err;
 		});
 	} catch (err) {
 		throw err;
 	} finally {
+		sourceFile?.close().catch((err) => {
+			throw err;
+		});
 		destFile?.close().catch((err) => {
 			throw err;
 		});
 	}
 };
 
+class InvalidURLError extends Error {}
 class RequestError extends Error {}
+class FileTypeError extends Error {}
+
+module.exports = downloadImage;
